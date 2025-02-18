b7e1b3a0850d7ec87a3027e17ef6c5dd80d33ed9
Kirens <dev@erik.work>
2025-02-18 08:42:23 +0100

    HACK: use cwd as LEAN_SRC_PATH

--- Main.lean
+++ Main.lean
@@ -24,16 +24,25 @@ open Lean
 
 /-- The main entry point. See `help` for more information on arguments. -/
 unsafe def main (args : List String) : IO UInt32 := do
-  initSearchPath (← findSysroot)
-  let srcSearchPath ← initSrcSearchPath
   match args with
   | [mainModuleName] =>
-    let mainModuleName := String.toName mainModuleName
-    enableInitializersExecution
-    let ast ← ASTExport.getASTForModule srcSearchPath mainModuleName
-    let stdout ← IO.getStdout
-    stdout.writeJson (toJson ast)
-    return 0
+    match (← IO.getEnv "LEAN_SRC_PATH") with
+    | none => /- TODO: imitate Lake.CLI.env more robustly -/
+      let child ← IO.Process.spawn {
+        cmd := (← IO.appPath).toString
+        args := #[ mainModuleName ]
+        env := #[ ("LEAN_SRC_PATH", some "./.") ]
+      }
+      child.wait
+    | _ => /- We have all necessary vars -/
+      initSearchPath (← findSysroot)
+      let srcSearchPath ← initSrcSearchPath
+      let mainModuleName := String.toName mainModuleName
+      enableInitializersExecution
+      let ast ← ASTExport.getASTForModule srcSearchPath mainModuleName
+      let stdout ← IO.getStdout
+      stdout.writeJson (toJson ast)
+      return 0
   | _ =>
     let stderr ← IO.getStderr
     stderr.putStrLn help
